9eb891e134f1d27b10f1b10aed99d3a74bec2c7f
sharmnilay <nilaysharma642@gmail.com>
2017-10-02 12:10:07 +0530

    Raju raand

---
+++ client/main.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
+import Login from './components/login/login'
+import './imports/startup/accounts-config.js';
+import AccountsUIWrapper from "./imports/ui/AccountsUIWrapper";
+
+class App extends Component {
+    render() {
+        return (
+            <div className="main">
+                <AccountsUIWrapper/>
+            </div>
+        )
+    }
+}
+
+Meteor.startup(() => {
+    ReactDOM.render(<App/>, document.querySelector('.container'))
+})
