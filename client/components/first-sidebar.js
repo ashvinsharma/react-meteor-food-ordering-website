6119659b8be7786973b5a1253048baf5a5f722e2
sharmnilay <nilaysharma642@gmail.com>
2017-10-05 11:36:51 +0530

    Navbar modifications

--- client/components/first-sidebar.js
+++ client/components/first-sidebar.js
@@ -1,6 +1,14 @@
 import React, {Component} from 'react'
 import {Col, Grid, ListGroup, ListGroupItem, Row} from 'react-bootstrap'
 import {Link} from 'react-router-dom'
+import FaHome from 'react-icons/lib/fa/home'
+import FaShop from 'react-icons/lib/fa/shopping-cart'
+import FaSale from 'react-icons/lib/fa/clipboard'
+import FaLine from 'react-icons/lib/fa/line-chart'
+import FaProd from 'react-icons/lib/fa/tags';
+import FaGrp from 'react-icons/lib/fa/group'
+import FaCom from  'react-icons/lib/fa/money'
+import FaSet from 'react-icons/lib/fa/cog'
 
 export default class FirstSidebar extends Component {
     render() {
@@ -10,14 +18,14 @@ export default class FirstSidebar extends Component {
                     <Row>
                         <Col md={2}>
                             <ListGroup>
-                                <ListGroupItem>Home</ListGroupItem>
-                                <ListGroupItem>Sell</ListGroupItem>
-                                <ListGroupItem>Sales Ledger</ListGroupItem>
-                                <ListGroupItem>Reporting</ListGroupItem>
-                                <Link to="/products"><ListGroupItem>Products</ListGroupItem></Link>
-                                <ListGroupItem>Customers</ListGroupItem>
-                                <ListGroupItem>ECommerce</ListGroupItem>
-                                <ListGroupItem>Setup</ListGroupItem>
+                                <ListGroupItem><FaHome />  Home</ListGroupItem>
+                                <ListGroupItem><FaShop/> Sell</ListGroupItem>
+                                <ListGroupItem><FaSale/> Sales Ledger</ListGroupItem>
+                                <ListGroupItem><FaLine/> Reporting</ListGroupItem>
+                                <Link to="/products"><ListGroupItem><FaProd/> Products</ListGroupItem></Link>
+                                <ListGroupItem><FaGrp/> Customers</ListGroupItem>
+                                <ListGroupItem><FaCom/> ECommerce</ListGroupItem>
+                                <ListGroupItem><FaSet/> Setup</ListGroupItem>
                             </ListGroup>
                         </Col>
                     </Row>
