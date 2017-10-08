d3fd58717f2fb79519f1cee52552270f7fef8893
ashvinsharma <ashvinsharma97@gmail.com>
2017-10-08 13:46:33 +0530

    fixed Product db structure, and Add New Product and Global Search instead of filter still to do insert delete and edit from table

--- client/components/web-register/products/products-list.js
+++ client/components/web-register/products/products-list.js
@@ -7,27 +7,38 @@ import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table'
 import {Products} from '../../../../imports/collections/products'
 
 class ProductsList extends Component {
+    handleInsertRow(row) {
+        console.log(row)
+    }
+
     getProducts() {
         return (
             <BootstrapTable data={this.props.products}
+                            insertRow={true}
+                            search={true}
                             selectRow={{
                                 mode: 'checkbox',
                                 hideSelectColumn: true,
                                 clickToSelect: true,
                                 bgColor: 'grey'
                             }}
+                            options={{
+                                clearSearch: true,
+                                afterInsertRow: this.handleInsertRow.bind(this)
+                            }}
                             pagination>
                 <TableHeaderColumn dataField='_id'
                                    isKey
                                    dataSort={true}
-                                   width='10%'>Product ID</TableHeaderColumn>
-                <TableHeaderColumn dataField='productName'
-                                   filter={{
-                                       type: 'TextFilter',
-                                       delay: 0
-                                   }} dataSort={true}>Product Name</TableHeaderColumn>
+                                   width='10%'
+                                   editable={false}
+                                   hidden>Product ID</TableHeaderColumn>
+                <TableHeaderColumn dataField='name'
+                                   dataSort={true}>Product Name</TableHeaderColumn>
+                <TableHeaderColumn dataField='description'>Description</TableHeaderColumn>
                 <TableHeaderColumn dataField='price'
                                    dataSort={true}>Product Price</TableHeaderColumn>
+                <TableHeaderColumn dataField='discount'>Discount</TableHeaderColumn>
             </BootstrapTable>
         )
     }
