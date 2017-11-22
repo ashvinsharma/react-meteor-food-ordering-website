8e919031308482ad30bc9a7f5bc1a94e33c15ed4
sharmnilay <nilaysharma642@gmail.com>
2017-11-22 22:45:29 +0530

    Cook DashBoard Finished. Cart Properties Improved.

---
+++ client/components/web-register/customer/feedback.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+import {createContainer} from 'meteor/react-meteor-data'
+import {Orders} from '../../../../imports/collections/orders'
+import {Accordion, Button, Col, Grid, Panel, Row} from 'react-bootstrap'
+import {BootstrapTable, ButtonGroup, TableHeaderColumn} from 'react-bootstrap-table'
+
+class Feedback extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hi!</h1>
+            </div>
+        )
+    }
+}
+
+export default Feedback
